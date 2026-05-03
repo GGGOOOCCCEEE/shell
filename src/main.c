39d54bf669d2f28d705a3c0ad8fde250b169dc61
GGGOOOCCCEEE <142725354+GGGOOOCCCEEE@users.noreply.github.com>
2026-05-03 23:36:54 +0800

    2026/5/3_3

--- src/main.c
+++ src/main.c
@@ -35,6 +35,20 @@ static bool find_in_path(const char *command, char *resolved_path, size_t resolv
   return false;
 }
 
+static int tokenize(char *input, char *argv[], int max_args) {
+  int argc = 0;
+  char *token = strtok(input, " \t");
+
+  while (token != NULL && argc < max_args - 1) {
+    argv[argc++] = token;
+    token = strtok(NULL, " \t");
+  }
+
+  argv[argc] = NULL;
+  return argc;
+}
+
+
 int main(int argc, char *argv[]) {
   const char *builtins[] = {"exit", "echo", "type"};
   char command[1024];
@@ -42,33 +56,48 @@ int main(int argc, char *argv[]) {
   while (1) {
 
     printf("$ ");
-    fgets(command, sizeof(command), stdin);
+    if (fgets(command, sizeof(command), stdin) == NULL) {
+      break;
+    }
     command[strcspn(command, "\n")] = '\0';
 
-    if (strcmp(command, "exit") == 0) {
+    char *args[128];
+    int arg_count = tokenize(command, args, 128);
+    if (arg_count == 0) {
+      continue;
+    }
+
+    if (strcmp(args[0], "exit") == 0) {
       break;
     } 
     
-    else if (strncmp(command, "echo ", 5) == 0) {
-      printf("%s\n", command + 5);
+    else if (strcmp(args[0], "echo") == 0) {
+      for (int i = 1; i < arg_count; i++) {
+        if (i > 1) {
+          printf(" ");
+        }
+        printf("%s", args[i]);
+      }
+      printf("\n");
     } 
     
-    else if (strncmp(command, "type ", 5) == 0) {
-      const char *arg = command + 5;
-      if (is_builtin(arg, builtins, sizeof(builtins) / sizeof(builtins[0]))) {
-        printf("%s is a shell builtin\n", arg);
+    else if (strcmp(args[0], "type") == 0) {
+      if (arg_count < 2) {
+        printf("\n");
+      } else if (is_builtin(args[1], builtins, sizeof(builtins) / sizeof(builtins[0]))) {
+        printf("%s is a shell builtin\n", args[1]);
       } else {
         char resolved_path[1024];
-        if (find_in_path(arg, resolved_path, sizeof(resolved_path))) {
-          printf("%s is %s\n", arg, resolved_path);
+        if (find_in_path(args[1], resolved_path, sizeof(resolved_path))) {
+          printf("%s is %s\n", args[1], resolved_path);
         } else {
-          printf("%s: not found\n", arg);
+          printf("%s: not found\n", args[1]);
         }
       }
     } 
-    
+
     else {
-      printf("%s: command not found\n", command);
+      printf("%s: command not found\n", args[0]);
     }
   }
 
